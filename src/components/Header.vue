2294cf6faebc9e048cc013aed3944f57621a2f9e
Denis Medvedev <gloomymed@gmail.com>
2020-12-08 22:27:19 +1000

    Initial project

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Coinstock Training</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">My actives</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Buy coins</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Balance: 10 000$
+    </span>
+  </div>
+</nav>
+</template>
